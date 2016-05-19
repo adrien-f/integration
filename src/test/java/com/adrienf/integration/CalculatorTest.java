8ad6fcb38f0026d9fe1a150d322030f7c0d2b704
Adrien Fillon <adrien.fillon+github@gmail.com>
2016-05-19 15:15:27 +0200

    Move square method to Calculator class and add tests

---
+++ src/test/java/com/adrienf/integration/CalculatorTest.java
@@ -0,0 +1,18 @@
+package com.adrienf.integration;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() throws Exception {
+        assertEquals(Calculator.add(1, 1), 2);
+    }
+
+    @Test
+    public void testSquare() throws Exception {
+        assertEquals(Calculator.square(2), 4);
+    }
+}
